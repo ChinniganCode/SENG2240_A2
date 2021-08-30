b95d6b128520dbb2a20abb6b20a892e79885ced8
Finbar Laffan <finbar.a.laffan@gmail.com>
2021-08-30 16:19:04 +1000

    Initial commit

---
+++ src/P1/Intersection.java
@@ -0,0 +1,15 @@
+package P1;
+
+import java.util.concurrent.Semaphore;
+
+public class Intersection {
+    private String name;
+    private int track1Crossed; //to get to 150
+    private int track2Crossed; //to get to 150
+    private Semaphore sem;
+
+    public Intersection() {
+
+    }
+
+}
---
+++ src/P1/WAR.java
@@ -0,0 +1,20 @@
+package P1;
+
+public class WAR {
+    private String id;
+    private String direction; // e.g. North, South, East, West
+    private String location; // e.g. S2, D1, D2, S2
+    private String track;
+    private boolean loaded;
+
+//SETTERS AND GETTERS
+
+    public void changeDirection() {
+
+    }
+    public void run() {
+
+    }
+
+
+}

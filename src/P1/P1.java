59e02e6938a5523002515ec4b193f0b633c10add
Finbar Laffan <finbar.a.laffan@gmail.com>
2021-08-30 18:12:15 +1000

    - Started on class building - Created WAR class with threads
    TODO: Implement semaphore

--- src/P1/P1.java
+++ src/P1/P1.java
@@ -1,8 +1,34 @@
 package P1;
 
-public class P1 {
+import java.io.File;
+import java.util.Scanner;
 
+public class P1 {
     public static void main(String[] args) {
-	// write your code here
+        String input;
+        int numWars = 0;
+        int warCount = 1;
+        String direction;
+        try {
+            File myObj = new File(args[0]); //file input
+            Scanner myReader = new Scanner(myObj);
+            while(myReader.hasNext()) {
+
+                input = myReader.next();
+                numWars = Integer.parseInt(input.substring(input.lastIndexOf("=" )+1));
+                for (int i = 0; i < numWars; i++) {
+                    WAR newWar = new WAR("WAR-" + warCount,input.charAt(0));
+                    System.out.println("WAR-" + warCount + " Direction: " +input.charAt(0));
+                    warCount++;
+                    newWar.start();
+                }
+            }
+            myReader.close();
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
     }
 }
--- src/P1/WAR.java
+++ src/P1/WAR.java
@@ -1,20 +1,95 @@
 package P1;
 
-public class WAR {
-    private String id;
-    private String direction; // e.g. North, South, East, West
-    private String location; // e.g. S2, D1, D2, S2
-    private String track;
-    private boolean loaded;
+/*TODO:
+- Read each WAR from the text file
+- Have each WAR run on a thread using implement Runnable or extends Thread
+- try to run indefinetely
+1. Announce e.g. "WAR-X (LOADED) Wating at the Intersection. Going towards destination
+2. attempt to get access to semaphore
+3. Once access aquired, announce Checkpoint 1, Sleep(200), Checkpoint 2, Sleep(200), Checkpoint 3, Sleep(200)
+4. Announce crossed e.g. "WAR-X (LOADED) cross the intersection"
+5. Change loaded, destination and location
+6. Print updated count (not in this class)
+*/
 
-//SETTERS AND GETTERS
+public class WAR extends Thread {
+    private String id; // i.e. WAR-X
+    private String destination; // e.g. Dock1, Dock2, Storage1, Storage2
+    private String location; // e.g. Dock1, Dock2, Storage1, Storage2
+    private String loaded; //whether the WAR is loaded or not, can be determined by location + destination
 
-    public void changeDirection() {
-
+    public WAR(String id, char startDirection) {
+        this.id = id;
+        setInitLocation(startDirection);
+        updateWAR();
     }
+
+    @Override
     public void run() {
-
+        while(true) {
+            System.out.println("WAR-" + id +" (" + loaded + ") "+"Waiting at the intersection. Going towards " + destination);
+            //Semaphore
+        }
+    }
+    public void updateWAR() {
+        switch(location) {
+            case "Storage1":
+                destination = "Dock1";
+                loaded = "LOADED";
+                break;
+            case "Storage2":
+                destination = "Dock2";
+                loaded = "LOADED";
+                break;
+            case "Dock1":
+                destination = "Storage1";
+                loaded = "UNLOADED";
+                break;
+            case "Dock2":
+                destination = "Storage2";
+                loaded = "UNLOADED";
+                break;
+        }
+    }
+    public void setInitLocation(char direction) {
+        switch(direction) {
+            case 'N':
+                location = "Storage2";
+                break;
+            case 'E':
+                location = "Dock1";
+                break;
+            case 'S':
+                location = "Dock2";
+                break;
+            case 'W':
+                location = "Storage1";
+                break;
+        }
     }
 
-
+    public void turnAround() {
+        switch (location) {
+            case "Storage1":
+                destination = "Storage1";
+                location = "Dock1";
+                loaded = "UNLOADED";
+                break;
+            case "Storage2":
+                destination = "Storage2";
+                location = "Dock1";
+                loaded = "UNLOADED";
+                break;
+            case "Dock1":
+                destination = "Dock1";
+                location = "Storage1";
+                loaded = "LOADED";
+                break;
+            case "Dock2":
+                destination = "Dock2";
+                location = "Storage2";
+                loaded = "LOADED";
+                break;
+        }
+    }
 }
